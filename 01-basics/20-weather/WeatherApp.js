377798cd49098f9d5162a5a246eb1a1b323dfc3d
Дмитрий Пашков <86959216+DmitryPash@users.noreply.github.com>
2024-07-17 09:20:20 +0300

    Выполнено задание - weather

--- 01-basics/20-weather/WeatherApp.js
+++ 01-basics/20-weather/WeatherApp.js
@@ -1,47 +1,65 @@
-import { defineComponent } from 'vue/dist/vue.esm-bundler.js'
+import { defineComponent, ref } from 'vue/dist/vue.esm-bundler.js'
 import { getWeatherData, WeatherConditionIcons } from './weather.service.ts'
 
 export default defineComponent({
   name: 'WeatherApp',
 
+  setup() {
+    const weatherData = ref(getWeatherData())
+    const weatherIcon = WeatherConditionIcons
+    const countTemp = temp => (temp - 273.15).toFixed(1)
+    const calcDay = (dt, sunrise, sunset) => {
+      return true ? dt < sunrise || dt > sunset : false
+    }
+    const calcPressure = pressure => {
+      return (pressure * 0.75).toFixed(0)
+    }
+    return {
+      weatherData,
+      countTemp,
+      weatherIcon,
+      calcDay,
+      calcPressure,
+    }
+  },
+
   template: `
     <div>
       <h1 class="title">Погода в Средиземье</h1>
-
       <ul class="weather-list unstyled-list">
-        <li class="weather-card weather-card--night">
-          <div class="weather-alert">
+        <li v-for="weather in weatherData" class="weather-card " :class="{'weather-card--night': calcDay(weather.current.dt, weather.current.sunrise, weather.current.sunset)}">
+          <div v-if="weather.alert" class="weather-alert">
             <span class="weather-alert__icon">⚠️</span>
-            <span class="weather-alert__description">Королевская метеослужба короля Арагорна II: Предвещается наступление сильного шторма.</span>
+            <span class="weather-alert__description">{{weather.alert.sender_name + ': ' + weather.alert.description}}</span>
           </div>
           <div>
             <h2 class="weather-card__name">
-              Гондор
+              {{ weather.geographic_name }}
             </h2>
             <div class="weather-card__time">
-              07:17
+             {{ weather.current.dt }}
             </div>
           </div>
           <div class="weather-conditions">
-            <div class="weather-conditions__icon" title="thunderstorm with heavy rain">⛈️</div>
-            <div class="weather-conditions__temp">15.0 °C</div>
+            <div class="weather-conditions__icon" :title="weather.current.weather.description">{{weatherIcon[weather.current.weather.id]}}</div>
+            <div class="weather-conditions__temp">{{ countTemp(weather.current.temp) }} °C</div>
           </div>
           <div class="weather-details">
             <div class="weather-details__item">
               <div class="weather-details__item-label">Давление, мм рт. ст.</div>
-              <div class="weather-details__item-value">754</div>
+              <div class="weather-details__item-value">{{ calcPressure(weather.current.pressure) }}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Влажность, %</div>
-              <div class="weather-details__item-value">90</div>
+              <div class="weather-details__item-value">{{ weather.current.humidity }}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Облачность, %</div>
-              <div class="weather-details__item-value">100</div>
+              <div class="weather-details__item-value">{{ weather.current.clouds }}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Ветер, м/с</div>
-              <div class="weather-details__item-value">10.5</div>
+              <div class="weather-details__item-value">{{ weather.current.wind_speed }}</div>
             </div>
           </div>
         </li>
