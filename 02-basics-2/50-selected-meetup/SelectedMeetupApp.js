6685159626c9bd9c9573cb9685138820e67f8594
Дмитрий Пашков <86959216+DmitryPash@users.noreply.github.com>
2024-08-19 11:05:07 +0300

    Маркировка емаил + выбор митапа

--- 02-basics-2/50-selected-meetup/SelectedMeetupApp.js
+++ 02-basics-2/50-selected-meetup/SelectedMeetupApp.js
@@ -1,75 +1,57 @@
-import { defineComponent } from 'vue'
-// import { getMeetup } from './meetupsService.ts'
+import { computed, defineComponent, onMounted, ref } from 'vue'
+import { getMeetup } from './meetupsService.ts'
+
+const inCart = (state, getters) => id => (getters.index(id) ? -1 : '')
 
 export default defineComponent({
   name: 'SelectedMeetupApp',
 
-  setup() {},
+  setup() {
+    const meetup = ref([])
+    const currentSlide = ref(1)
+
+    const resultSlide = computed(() => {
+      return currentSlide
+    })
+
+    onMounted(async () => {
+      try {
+        for (let i = 1; i <= 5; i++) meetup.value.push(await getMeetup(i))
+      } catch (error) {
+        console.error(error)
+      }
+    })
+
+    return {
+      meetup,
+      currentSlide,
+      resultSlide,
+    }
+  },
 
   template: `
     <div class="meetup-selector">
       <div class="meetup-selector__control">
-        <button class="button button--secondary" type="button" disabled>Предыдущий</button>
-
+        <button @click="currentSlide--" class="button button--secondary" type="button" :disabled="currentSlide <= 1">Предыдущий</button>
         <div class="radio-group" role="radiogroup">
-          <div class="radio-group__button">
+          <div v-for="page of meetup" class="radio-group__button">
             <input
-              id="meetup-id-1"
+              :id="'meetup-id-' + page.id"
               class="radio-group__input"
               type="radio"
               name="meetupId"
-              value="1"
+              :value="page.id"
+              v-model="currentSlide"
             />
-            <label for="meetup-id-1" class="radio-group__label">1</label>
-          </div>
-          <div class="radio-group__button">
-            <input
-              id="meetup-id-2"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              value="2"
-            />
-            <label for="meetup-id-2" class="radio-group__label">2</label>
-          </div>
-          <div class="radio-group__button">
-            <input
-              id="meetup-id-3"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              value="3"
-            />
-            <label for="meetup-id-3" class="radio-group__label">3</label>
-          </div>
-          <div class="radio-group__button">
-            <input
-              id="meetup-id-4"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              value="4"
-            />
-            <label for="meetup-id-4" class="radio-group__label">4</label>
-          </div>
-          <div class="radio-group__button">
-            <input
-              id="meetup-id-5"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              value="5"
-            />
-            <label for="meetup-id-5" class="radio-group__label">5</label>
+            <label :for="'meetup-id-' + page.id" class="radio-group__label">{{ page.id }}</label>
           </div>
         </div>
 
-        <button class="button button--secondary" type="button">Следующий</button>
+        <button @click="currentSlide++" :disabled="currentSlide >= meetup.length" class="button button--secondary" type="button">Следующий</button>
       </div>
-
-      <div class="meetup-selector__cover">
+      <div v-for="(slide, index) of meetup" v-show="index === currentSlide - 1" class="meetup-selector__cover">
         <div class="meetup-cover">
-          <h1 class="meetup-cover__title">Some Meetup Title</h1>
+          <h1 class="meetup-cover__title">{{ slide.title }}</h1>
         </div>
       </div>
 
